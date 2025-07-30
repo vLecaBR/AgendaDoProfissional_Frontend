9462acc4b8b9cfd5920c56bc10e6d43410546e8b
vlecabr <vitartasleca@gmail.com>
2025-07-30 11:28:34 -0300

    adicionado seção de artista

---
+++ src/components/ArtistsSection/ArtistsSection.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  Section,
+  SectionTitle,
+  ArtistsGrid,
+  ArtistCard,
+  ArtistImage,
+  ArtistName,
+  ArtistBio
+} from './ArtistsSection.styles';
+
+import artists from '../../pages/Home/artists'; // ou ajuste o caminho conforme onde o JSON estiver
+
+export default function ArtistsSection() {
+  return (
+    <Section>
+      <SectionTitle>Nossos Artistas</SectionTitle>
+      <ArtistsGrid>
+        {artists.map((artist, index) => (
+          <ArtistCard key={index}>
+            <ArtistImage src={artist.img} alt={artist.name} />
+            <ArtistName>{artist.name}</ArtistName>
+            <ArtistBio>{artist.bio}</ArtistBio>
+          </ArtistCard>
+        ))}
+      </ArtistsGrid>
+    </Section>
+  );
+}
